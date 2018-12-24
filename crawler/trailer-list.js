12e9d0db9188f142fb85659a6ae5341dca589161
20170228-211617\Administrator <dakani@outlook.com>
2018-12-24 10:16:32 +0800

    爬虫列表

---
+++ crawler/trailer-list.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 500,
+    defaultViewport: {
+      width: 1366,
+      height: 768
+    }
+  });
+  const page = await browser.newPage();
+  const url = 'https://movie.douban.com/explore#!type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=0';
+  await page.goto(url, {
+    waitUntil: 'domcontentloaded'
+  });
+
+  const moreClk = async(page) => {
+    console.log(1111111111111)
+    try {
+      console.log(2222222222)
+      await page.waitFor('.more');
+      await page.click('.more');
+      await page.waitFor(5000);
+      await moreClk(page);
+    } catch (e) {
+      console.log(33333333333)
+    }
+  }
+  await moreClk(page); 
+})()
